f2938d90750eae61355b4ee3583f96686756ef87
m9d <1010.oekaki@gmail.com>
2013-07-14 15:16:14 +0900

    effect
    画面をタッチした際にエフェクトが表示されるように設定中

--- 3-4_1.js
+++ 3-4_1.js
@@ -5,7 +5,7 @@ window.onload = function() {
     var game = new Game(320, 320);
     game.fps = 16;    
     //画像の読み込み
-    game.preload('hae16.png');
+    game.preload('hae16.png','ti.png');
     game.tick = 0;
     var time = 32;
     
@@ -13,8 +13,8 @@ window.onload = function() {
         return Math.floor(Math.random() * num);
     }
 	
-    var Fly = Class.create(Sprite, // Spriteクラスを継承
-        { initialize:function(x,y){ //初期化する
+    var Fly = Class.create(Sprite,{
+        initialize:function(x,y){ //初期化する
             Sprite.call(this,16,16); //Spriteオブジェクトを初期化
             this.image = game.assets['hae16.png'];
             this.x = x;
@@ -28,7 +28,17 @@ window.onload = function() {
     
     //ロード完了時に呼ばれる
     game.onload = function(){
-    game.addEventListener(Event.ENTER_FRAME,function(){
+        /*var effect = new Sprite(36, 36);
+            effect.image = game.assets['ti.png'];
+            effect.addEventListener(Event.TOUCH_START,function(e){
+                effect.x = e.x;
+                effect.y = e.y;
+                effect
+                game.rootScene.addChild(effect);
+            });
+            game.rootScene.addChild(effect);
+        }*/
+        game.addEventListener(Event.ENTER_FRAME,function(){
         game.tick++;
         if(game.tick % time == 0){
             var fly = new Fly(rand(200),rand(200));
@@ -36,9 +46,9 @@ window.onload = function() {
         } else if(game.tick % 32 == 0){
             var fly = new Fly(rand(200),rand(200));
         }
-        
+       
     });
-    };
+    }
     //ゲームの開始
     game.start();
-};
+}
